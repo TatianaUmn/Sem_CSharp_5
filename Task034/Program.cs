00d9bbda3273d37d674738a39e2aef596e982ab2
Tatiana <tatum@rambler.ru>
2022-08-23 14:09:43 +0300

    Task 034

---
+++ Task034/Program.cs
@@ -0,0 +1,42 @@
+﻿// // Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных
+// чисел в массиве.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNumbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int[] array = CreateArray(10, 100, 1000);// ограничила массив 10 положительными трехзначными цмфрами
+
+PrintArray(array);
+
+int even = EvenNumbers(array);
+Console.WriteLine($"Количество чётных чисел в массиве: {even}");
